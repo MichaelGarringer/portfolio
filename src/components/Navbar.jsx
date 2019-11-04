0203410ed8d96a7ceb9e00d46a9f8976424a1bdc
Mike Garringer <michaelleelyngarringer@gmail.com>
2019-11-04 13:46:20 -0600

    changes to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,12 +11,12 @@ function Navbar() {
 
 <nav class="navbar navbar-expand-lg navbar-light">
   <Link to="/" className="navbar-brand"><h2>Mike Garringer</h2></Link>
-  <ul class="navbar-nav justify-content-end">
-   
+  <ul class="navbar-nav ml-auto">
     <li class="nav-item">
-    <Link to="/resume" className={window.location.pathname === "/resume" ? "nav-link active" : "nav-link"}><h5>Resume</h5></Link>
+      <Link to="/resume" className="resume>"><h5>Resume</h5></Link>
     </li>
   </ul>
+  
 </nav>
 
     </>
